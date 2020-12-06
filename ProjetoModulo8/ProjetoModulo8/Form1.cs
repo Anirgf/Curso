765453b0d2c48a48a50ed3d8ddaf28fcc51f1fa4
Anirgf <61605200+Anirgf@users.noreply.github.com>
2020-12-06 17:20:32 -0300

    Banco de dados

--- ProjetoModulo8/ProjetoModulo8/Form1.cs
+++ ProjetoModulo8/ProjetoModulo8/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace ProjetoModulo8
 {
@@ -15,6 +17,12 @@ namespace ProjetoModulo8
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            MySqlConnection conexao = new MySqlConnection(conn);
         }
     }
 }
